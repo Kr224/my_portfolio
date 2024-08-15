ce33c43d25808530e638f8ac4695ce3991c7c611
Karishni <Karishni.Gupta@dal.ca>
2024-08-15 17:30:16 -0300

    Feat: Fixing code for banner and adding icon

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../Assets/images/me.png";
 import TrackVisibility from 'react-on-screen';
@@ -10,7 +9,13 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "A Programmer", "Tech enthusiast", "Lifelong learner" ];
+  const butterflyIcon = "https://img.icons8.com/?size=100&id=IKi8N-JbvZt2&format=png&color=000000";
+
+  const toRotate = [
+    "Hello there!",
+    "I am Karishni"
+  ];
+
   const period = 2000;
 
   useEffect(() => {
@@ -18,8 +23,8 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => clearInterval(ticker);
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
@@ -44,32 +49,43 @@ export const Banner = () => {
     } else {
       setIndex(prevIndex => prevIndex + 1);
     }
-  }
+  };
 
   return (
-      <section className="banner">
-        <Container>
-          <Row className="aligh-items-center">
-            <Col xs={12} md={6} xl={7}>
-              <TrackVisibility>
-                {({ isVisible }) =>
-                    <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                      <span className="tagline">Welcome to my Portfolio</span>
-                      <h1>{`Hi! I'm Karishni`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "A Programmer", "Tech enthusiast", "Lifelong learner" ]'><span className="wrap">{text}</span></span></h1>
-                      <p>I am passionate Computer Science student currently pursuing my degree at Dalhousie University. I am dedicated to expanding my knowledge and skills in various programming languages and technologies. Feel free to explore my portfolio to see some of the projects I’ve worked on and get a glimpse of my journey in the tech world. I’m excited to connect with like-minded individuals!</p>
-                    </div>}
-              </TrackVisibility>
-            </Col>
-            <Col xs={12} md={6} xl={5}>
-              <TrackVisibility>
-                {({ isVisible }) =>
-                    <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                      <img src={headerImg} alt="Header Img"/>
-                    </div>}
-              </TrackVisibility>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-  )
-}
+    <section className="banner">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    <span className="txt-rotate">
+                      <span className="wrap">{text}</span>
+                    </span>
+                    {text.includes("Karishni") && (
+                      <span className="butterfly-container">
+                        <img src={butterflyIcon} alt="butterfly" className="butterfly-icon" />
+                      </span>
+                    )}
+                  </h1>
+                  <p>
+                    I am a Computer Science student currently pursuing my degree at Dalhousie University. I am dedicated to expanding my knowledge and skills in various programming languages and technologies. Feel free to explore my portfolio to see some of the projects I’ve worked on and get a glimpse of my journey in the tech world. I’m excited to connect with like-minded individuals!
+                  </p>
+                </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img" />
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
